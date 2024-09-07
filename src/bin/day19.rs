747b302f9dea3556d63a97f9446335ae8f59b3d6
Douglas Boyle <douglas@maisonboyle.com>
2024-09-08 00:34:31 +0100

    Simplify day 19, just do in one top-down pass

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -1,7 +1,6 @@
 use std::{cmp::{max, min}, collections::HashMap, io::{Error, ErrorKind}, ops::{Deref, DerefMut, Range}, str::FromStr};
 
 use enum_map::{Enum, EnumMap};
-use rust_aoc::TopologicalSort;
 
 fn main() {
     let mut lines = rust_aoc::read_input(19);
@@ -15,35 +14,25 @@ fn main() {
         .map(|part| part.sum_value()).sum();
     println!("Part 1: Total {total}"); // 319062
 
-    let mut sort = WorkflowTopologicalSort { workflows: &workflows };
-    let sorted = sort.sort();
-    let sorted = if let Ok(sorted) = sorted { sorted } else { panic!("Not a DAG") };
-
-    // Calculate in reverse DAG order
-    let mut accepted_ranges: HashMap<&str, PartRange> = HashMap::new();
-    accepted_ranges.insert("A", PartRange::all());
-    
-
-    for id in sorted.into_iter().rev() {
+    let mut total: usize = 0;
+    let mut stack = vec![("in", RatingsIntervals::all())];
+    while let Some((id, mut range)) = stack.pop() {
         let rules = &workflows.get(id).unwrap().rules;
-        let mut previous_rules = PartRange::all(); // subtractive, constrained as we get further down non-matching rules
-        let mut overall_range = PartRange::none(); // additive, paths via each rule
         for rule in rules {
-            let destination = rule.destination();
-            if let Some(destination_range) = accepted_ranges.get(&destination[..]) {
-                let rule_range = rule.requirements();
-                let previous_and_this_rule_range = previous_rules.intersect(&rule_range);
-                let matching_range = previous_and_this_rule_range.intersect(destination_range);
-                if !matching_range.is_empty() {
-                    overall_range.include(matching_range);
+            let matching_range = range.intersect(&rule.requirements());
+            if !matching_range.is_empty() {
+                let destination = rule.destination();
+                if destination == "A" {
+                    total += matching_range.size();
+                } else if destination != "R" {
+                    stack.push((destination, matching_range));
                 }
             }
-            previous_rules = previous_rules.intersect(&rule.negated_requirements());
+            range = range.intersect(&rule.negated_requirements());
+            if range.is_empty() { break }
         }
-        if !overall_range.is_empty() { accepted_ranges.insert(id, overall_range); }
     }
-    
-    let total: usize = accepted_ranges.remove("in").unwrap().ranges.iter().map(|range| range.size()).sum();
+
     println!("Part 2: Total {total}"); // 118638369682135
 }
 
@@ -61,107 +50,38 @@ fn process(part: &Part, workflows: &HashMap<String, Workflow>) -> bool {
     }
 }
 
-struct WorkflowTopologicalSort<'a> {
-    workflows: &'a HashMap<String, Workflow>,
-}
-
-impl TopologicalSort for WorkflowTopologicalSort<'_> {
-    type Node = String;
-
-    fn get_all_nodes(&self) -> Vec<&Self::Node> {
-        self.workflows.keys().collect()
-    }
-
-    fn get_edges(&self, node: &Self::Node) -> Vec<&Self::Node> {
-        self.workflows.get(node).unwrap().downstream_nodes().into_iter()
-            .filter(|node| *node != "A" && *node != "R")
-            .collect()
-    }
-}
-
 const MIN_INCLUSIVE: i32 = 1;
 const MAX_EXCLUSIVE: i32 = 4001;
 const FULL_RANGE: Range<i32> = MIN_INCLUSIVE..MAX_EXCLUSIVE;
 
-// Overall type, 1 per node, each RatingsIntervals has different dimensions that prevent merging them
-#[derive(Clone)]
-struct PartRange {
-    ranges: Vec<RatingsIntervals>
-}
-
-impl std::fmt::Debug for PartRange {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.ranges.fmt(f)
-    }
-}
-
-impl PartRange {
-    fn all() -> PartRange {
-        PartRange::single(RatingsIntervals::all())
-    }
-
-    fn single(ratings: RatingsIntervals) -> PartRange {
-        PartRange { ranges: vec![ratings] }
-    }
-
-    fn none() -> PartRange {
-        PartRange { ranges: vec![] }
-    }
-
-    fn intersect(&self, other: &PartRange) -> PartRange {
-        // naive pairwise intersections
-        self.ranges.iter().flat_map(|range| 
-            other.ranges.iter().map(|other_range| range.intersect(other_range))
-        ).filter(|range| !range.is_empty()).collect()
-    }
-
-    fn include(&mut self, mut other: PartRange) {
-        // TODO: Very naive, would ideally merge compatible ranges
-        self.ranges.append(&mut other.ranges);
-    }
-}
-
-impl Deref for PartRange {
-    type Target = Vec<RatingsIntervals>;
-    
-    fn deref(&self) -> &Self::Target {
-        &self.ranges
-    }
-}
-
-impl DerefMut for PartRange {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.ranges
-    }
-}
-
-impl FromIterator<RatingsIntervals> for PartRange {
-    fn from_iter<T: IntoIterator<Item = RatingsIntervals>>(iter: T) -> Self {
-        PartRange { ranges: iter.into_iter().collect() }
-    }
-}
-
-// 1 combination of ratings, each category muust match the interval for that category
 #[derive(Clone)]
 struct RatingsIntervals {
-    ratings: EnumMap<Category, Intervals>
+    ratings: EnumMap<Category, Range<i32>>
 }
 
 impl RatingsIntervals {
     fn all() -> RatingsIntervals {
-        RatingsIntervals { ratings: EnumMap::from_fn(|_| Intervals::all()) }
+        RatingsIntervals::from_fn(|_| FULL_RANGE)
+    }
+
+    fn none() -> RatingsIntervals {
+        RatingsIntervals::from_fn(|_| 0..0)
     }
 
     fn intersect(&self, other: &RatingsIntervals) -> RatingsIntervals {
-        RatingsIntervals { ratings: EnumMap::from_fn(|cat| self.ratings[cat].intersect(&other.ratings[cat])) }
+        RatingsIntervals::from_fn(|cat| intersect(&self.ratings[cat], &other.ratings[cat]))
+    }
+
+    fn from_fn<F: FnMut(Category) -> Range<i32>>(f: F) -> RatingsIntervals {
+        RatingsIntervals { ratings: EnumMap::from_fn(f) }
     }
 
     fn is_empty(&self) -> bool {
-        self.ratings.values().any(Intervals::is_empty)
+        self.ratings.values().any(Range::is_empty)
     }
 
     fn size(&self) -> usize {
-        self.ratings.values().map(|intervals| intervals.size()).product()
+        self.ratings.values().map(|range| (range.end - range.start) as usize).product()
     }
 }
 
@@ -172,7 +92,7 @@ impl std::fmt::Debug for RatingsIntervals {
 }
 
 impl Deref for RatingsIntervals {
-    type Target = EnumMap<Category, Intervals>;
+    type Target = EnumMap<Category, Range<i32>>;
     
     fn deref(&self) -> &Self::Target {
         &self.ratings
@@ -182,59 +102,6 @@ impl Deref for RatingsIntervals {
 impl DerefMut for RatingsIntervals {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.ratings
-    }
-}
-
-// non-adjacent/overlapping ranges in increasing order, for a single category
-#[derive(Clone, Eq, PartialEq)]
-struct Intervals {
-    ranges: Vec<Range<i32>>
-}
-
-impl std::fmt::Debug for Intervals {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.ranges.fmt(f)
-    }
-}
-
-impl Intervals {
-    fn all() -> Intervals {
-        Intervals::single(FULL_RANGE)
-    }
-
-    fn single(range: Range<i32>) -> Intervals {
-        Intervals { ranges: vec![range] }
-    }
-
-    fn intersect(&self, other: &Intervals) -> Intervals {
-        // Could be smarter about this and walk down both intervals in sync,
-        // but instead we just take the intersection of each range from other
-        // with all of our own interval (in each case producing 1-2 results)
-        other.ranges.iter()
-            .flat_map(|range| self.intersect_range(range).ranges.into_iter())
-            .collect()
-    }
-
-    fn intersect_range(&self, range: &Range<i32>) -> Intervals {
-        self.ranges.iter().map(|r| intersect(r, range))
-            .filter(|range| !Range::is_empty(range))
-            .collect()
-    }
-
-    fn is_empty(&self) -> bool {
-        self.ranges.iter().all(Range::is_empty)
-    }
-
-    fn size(&self) -> usize {
-        self.ranges.iter().map(|range| range.end - range.start).sum::<i32>() as usize
-    }
-
-    // TODO: Currently never unioned, so always increasing!
-}
-
-impl FromIterator<Range<i32>> for Intervals {
-    fn from_iter<T: IntoIterator<Item = Range<i32>>>(iter: T) -> Self {
-        Intervals { ranges: iter.into_iter().collect() }
     }
 }
 
@@ -273,23 +140,6 @@ impl Workflow {
     fn process(&self, part: &Part) -> &String {
         self.rules.iter().filter(|rule| rule.matches(part)).next().unwrap().destination()
     }
-
-    fn downstream_nodes(&self) -> Vec<&String> {
-        self.rules.iter().map(|rule| rule.destination()).collect()
-    }
-
-    // TODO: Move from main loop to here?
-    /* 
-    fn downstream_requirements(&self) -> Vec<(String, Vec<PartRange>)> {
-        let mut result = Vec::new();
-        let requirements = PartRange::all();
-        for rule in &self.rules {
-            let destination = rule.destination();
-            let rule_requirement = rule.requirements();
-        }
-        result
-    }
-    */
 }
 
 impl FromStr for Workflow {
@@ -325,33 +175,33 @@ impl Rule {
         }
     }
 
-    fn requirements(&self) -> PartRange {
-        let mut ranges = RatingsIntervals::all();
+    fn requirements(&self) -> RatingsIntervals {
+        let mut ratings = RatingsIntervals::all();
         match self {
             Rule::Less(category, threshold, _) => {
-                ranges[*category] = Intervals::single(MIN_INCLUSIVE..*threshold);
+                ratings[*category] = MIN_INCLUSIVE..*threshold;
             },
             Rule::Greater(category, threshold, _) => {
-                ranges[*category] = Intervals::single((threshold+1)..MAX_EXCLUSIVE);
+                ratings[*category] = (threshold+1)..MAX_EXCLUSIVE;
             }
             Rule::Always(_) => {},
         }
-        PartRange::single(ranges)
+        ratings
     }
 
-    fn negated_requirements(&self) -> PartRange {
+    fn negated_requirements(&self) -> RatingsIntervals {
         match self {
             Rule::Less(category, threshold, _) => {
-                let mut ranges = RatingsIntervals::all();
-                ranges[*category] = Intervals::single(*threshold..MAX_EXCLUSIVE);
-                PartRange::single(ranges)
+                let mut ratings = RatingsIntervals::all();
+                ratings[*category] = *threshold..MAX_EXCLUSIVE;
+                ratings
             },
             Rule::Greater(category, threshold, _) => {
-                let mut ranges = RatingsIntervals::all();
-                ranges[*category] = Intervals::single(0..(threshold+1));
-                PartRange::single(ranges)
+                let mut ratings = RatingsIntervals::all();
+                ratings[*category] = 0..(threshold+1);
+                ratings
             },
-            Rule::Always(_) => PartRange::none(),
+            Rule::Always(_) => RatingsIntervals::none(),
         }
     }
 }
@@ -396,6 +246,5 @@ impl TryFrom<char> for Category {
 fn intersect(a: &Range<i32>, b: &Range<i32>) -> Range<i32> {
     let start = max(a.start, b.start);
     let end = min(a.end, b.end);
-  //  println!("{a:?} ^ {b:?} = {:?}", start..end);
     start..end
 }
