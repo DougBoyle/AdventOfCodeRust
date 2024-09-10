0f9bcb5fa963ba278d4f4ff9477d4d12685ef531
Douglas Boyle <douglas@maisonboyle.com>
2024-09-10 23:21:29 +0100

    Day 22: 3D points, sorting collection to do updates efficiently, graph theory Dominators (from compiler course) to work out which elements impact other ones

---
+++ src/bin/day22.rs
@@ -0,0 +1,194 @@
+use std::{collections::HashSet, io::Error, ops::{Add, Neg}, str::FromStr};
+
+
+fn main() {
+    let mut bricks: Vec<Brick> = rust_aoc::read_input(22).map(|s| s.parse().unwrap()).collect();
+    // sort by (start) z coordinate, then just need to look at earlier bricks to simulate each brick falling, and never revisit
+    bricks.sort_by_key(|p| p.start.z);
+
+    let down = -Axis::Z.as_vec();
+
+    println!("Num bricks: {}", bricks.len());
+
+    let mut falling_steps = 0;
+    for i in 0..bricks.len() {
+        let mut brick = bricks[i];
+        while brick.start.z > 1 {
+            let new_brick = brick + down;
+            let collides = (0..i).rev().any(|j| bricks[j].intersects(&new_brick));
+            if collides {
+                break;
+            } else {
+                brick = new_brick;
+                falling_steps += 1;
+            }
+        }
+        bricks[i] = brick;
+    }
+
+    println!("Complete! {falling_steps} falling steps");
+
+    // For each brick, work out which ones it is supported by
+    let supporters_of_bricks: Vec<HashSet<usize>> = (0..bricks.len()).map(|i| {
+        let brick = bricks[i];
+        let moved_brick = brick + down;
+        (0..i).filter(|&j| i != j && bricks[j].intersects(&moved_brick)).collect()
+    }).collect();
+
+    let lone_supporters: HashSet<_> = supporters_of_bricks.iter()
+        .filter(|set| set.len() == 1)
+        .flat_map(|set| set.iter()).collect();
+
+    println!("Of {} bricks, {} are the only ones supporting some other brick, {} could be removed",
+        bricks.len(), lone_supporters.len(), bricks.len() - lone_supporters.len()); // 395
+
+    // Already sorted by height, so just build a map of removing X -> set of other bricks that fall.
+    // Working backwards (top down),     
+
+
+    // Working out how many other bricks fall if one brick is removed resembles the problem of Dominators in graph theory.
+    // 1. For each brick i track the dominators of i:
+    //      Includes i
+    //      Includes the intersection of the dominators of each brick supporting i, i.e. bricks that make every supporter fall
+    // 2. No need to build reverse mapping, rather than counting 'how many bricks fall when brick i is removed', that's
+    //    the same as counting 'how many different bricks can be removed to make brick j fall'
+    // Bricks already tracked in height order, so just needs one forward pass to calculate (1)
+
+    let mut dominators: Vec<HashSet<usize>> = vec![];
+    for i in 0..bricks.len() {
+        let mut dominators_i = HashSet::new(); 
+        dominators_i.insert(i);
+
+        // intersection across all of the sets
+        let supporter_dominators: Vec<&HashSet<usize>> = supporters_of_bricks[i].iter().map(|j| &dominators[*j]).collect();
+        if !supporter_dominators.is_empty() {
+            let first_supporter_dominators = supporter_dominators.iter().next().unwrap();
+            for dominator in first_supporter_dominators.iter()
+                .filter(|dominator| supporter_dominators.iter().all(|dominators| dominators.contains(dominator))) {
+                    dominators_i.insert(*dominator);
+            }
+        }
+
+        dominators.push(dominators_i);
+    }
+
+    println!("Calculated dominators");
+
+    // no need to calculate reverse mapping, just look at number of dominator relations, subtracting each brick dominating itself
+    let total_strict_dominators: usize = dominators.iter().map(|dominators| dominators.len() - 1).sum();
+    println!("Total chain reaction bricks for each brick: {total_strict_dominators}"); // 64714
+}
+
+
+
+// Always a straight line i.e. only 1 coordinate differs.
+// To simplify handling, always sort points lexicographically so that start < end
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+struct Brick {
+    start: Point3, // inclusive at both ends
+    end: Point3,
+}
+
+impl Brick {
+    fn project(&self, axis: Axis) -> (i32, i32) {
+        (self.start.project(axis), self.end.project(axis))
+    }
+
+    // exploiting the fact all bricks are axis-aligned straight lines,
+    // we can just check if their start-end overlap in all 3 dimensions.
+    fn intersects(&self, brick: &Brick) -> bool {
+        Axis::all().into_iter().all(|axis| {
+            let (a_start, a_end) = self.project(axis);
+            let (b_start, b_end) = brick.project(axis);
+            lines_touch(a_start, a_end, b_start, b_end)
+        })
+    }
+}
+
+impl Add<Point3> for Brick {
+    type Output = Brick;
+
+    fn add(self, rhs: Point3) -> Self::Output {
+        Brick { start: self.start + rhs, end: self.end + rhs }
+    }
+}
+
+impl FromStr for Brick {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (first, second) = rust_aoc::split_in_two(s, '~');
+        let (first, second) = (first.parse().unwrap(), second.parse().unwrap());
+        let (start, end) = if first < second { (first, second) } else { (second, first) };
+        Ok(Brick { start, end })
+    }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, PartialOrd, Ord, Debug)]
+struct Point3 { x: i32, y: i32, z: i32 }
+
+impl Point3 {
+    fn project(&self, axis: Axis) -> i32 {
+        match axis {
+            Axis::X => self.x,
+            Axis::Y => self.y,
+            Axis::Z => self.z,
+        }
+    }
+}
+
+impl Add for Point3 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Point3 { x: self.x + rhs.x, y: self.y + rhs.y, z: self.z + rhs.z }
+    }
+}
+
+impl Neg for Point3 {
+    type Output = Point3;
+
+    fn neg(self) -> Self::Output {
+        Point3 { x: -self.x, y: -self.y, z: -self.z }
+    }
+}
+
+impl FromStr for Point3 {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let axes: Vec<i32> = s.split(',').map(|s| s.parse().unwrap()).collect();
+        assert!(axes.len() == 3);
+        Ok(Point3 { x: axes[0], y: axes[1], z: axes[2] })
+    }
+}
+
+// inclusive at endpoints i.e. an L shape counts
+fn lines_touch(a_start: i32, a_end: i32, b_start: i32, b_end: i32) -> bool {
+    !disjoint_lines(a_start, a_end, b_start, b_end)
+}
+
+fn disjoint_lines(a_start: i32, a_end: i32, b_start: i32, b_end: i32) -> bool {
+    a_start > b_end || b_start > a_end
+}
+
+#[derive(Eq, PartialEq, Clone, Copy, Debug)]
+enum Axis {
+    X, Y, Z
+}
+
+impl Axis {
+    fn as_vec(&self) -> Point3 {
+        match self {
+            Axis::X => Point3 { x: 1, y: 0, z: 0 },
+            Axis::Y => Point3 { x: 0, y: 1, z: 0 },
+            Axis::Z => Point3 { x: 0, y: 0, z: 1 },
+        }
+    }
+}
+
+impl Axis {
+    fn all() -> [Axis; 3] {
+        [Axis::X, Axis::Y, Axis::Z]
+    }
+}
